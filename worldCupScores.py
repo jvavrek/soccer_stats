985b4deb07d3a96f0c0eacc9206c6afceff621b2
Jayson Vavrek <jvavrek@mit.edu>
2017-11-22 17:05:18 -0500

    model updates

--- worldCupScores.py
+++ worldCupScores.py
@@ -5,6 +5,7 @@ import sys
 import numpy as np
 import scipy as sp
 import scipy.misc
+import scipy.special
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import linear_model
@@ -42,9 +43,13 @@ def prob_bivariate_poisson(lambda0, lambda1, lambda2, x, y):
   return prefactor1 * prefactor2 * sumfactor
 
 
-# FIXME unsure if this uses the marginal means or actual parameters
-def prob_diff_bivariate_poisson():
-  pass
+# FIXME unsure if this uses the marginal means or actual parameters.
+# Seems to use actual parameters
+def prob_diff_bivariate_poisson(lambda1, lambda2, z):
+  factor1 = np.exp(-(lambda1+lambda2))
+  factor2 = np.pow((lambda1/lambda2),z/2.0)
+  factor3 = sp.special.iv(z, 2.0*np.sqrt(lambda1*lambda2) )
+  return factor1 * factor2 * factor3
 
 
 # Build a table of BVP probabilities if necessary
@@ -58,12 +63,12 @@ def build_bivariate_poisson_table(lambda0, lambda1, lambda2, nmax=10):
 
 # (Currently skeleton) code for various regressions.
 # Ideally the way this should work is if scoreMatrix is nx2, this should regress on the BVP;
-# if scoreMatrix is nx1 (for score _differences_), it should regress on the diffBVP;
+# if scoreMatrix is nx1 (for score _differences_), it should regress on the BVPD;
 # should also be able to regress (for both cases) on the independent Poisson model
 # This means the heavy lifting of slicing the dataframe should be done in another function.
-#                  BVP   IP
-# scoreMatrix      nx2  nx1
-# parameters out     3    2
+#                  BVP   IP  BVPD
+# scoreMatrix      nx2  nx2   nx1
+# parameters out     3    2     2
 def score_regression(featureMatrix, scoreMatrix, opt='linear', alpha=0.5):
   reg = None
   if opt == 'linear':
@@ -78,11 +83,28 @@ def score_regression(featureMatrix, scoreMatrix, opt='linear', alpha=0.5):
   reg.fit(featureMatrix, scoreMatrix) # FIXME can pass n_jobs parameter > 1 if too slow
   return reg
 
+
+# Wrapper function to get the lambda parameters from the regression, breaking them apart.
+# NOTE: The regression should return log(lambda). As a result, using the predict()
+# method might not work.
+def get_lambda_params(regression):
+   params = regression.get_params()
+  nparams = len(params)
+
+  if nparams == 2: # independent Poisson
+    return np.exp(params[0]), np.exp(params[1])
+  elif nparams == 3: # bivariate Poisson
+    return np.exp(params[0]), np.exp(params[1]), np.exp(params[2])
+  else:
+    print "Error: bad number of parameters %d"%nparams
+    return None
+
 # TODO:
 # Understand different parameters better
 # Use the BVP to predict the winner of a game
 # Build a tournament structure table and propagate
 # MC sample initial distribution
+# Better error handling?
 
 #t = build_bivariate_poisson_table(0.2,2.1,3.2)
 #print t
